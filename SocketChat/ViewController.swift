51dc14d3e9751873a72595e87a868b1cf377a1b5
DLinZheHao <BDLinOrsWJ0528@gmail.com>
2023-09-08 10:32:34 +0800

    initialization

---
+++ SocketChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocketChat
+//
+//  Created by 林哲豪 on 2023/9/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
